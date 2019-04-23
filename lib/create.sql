392475b0a0520a1e6386c8b92e7bbdc3a41d57a8
ADIL NAIMI <adilnm23@hotmail.com>
2019-04-23 07:54:23 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,8 +2,8 @@ CREATE TABLE bears (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER,
-  gender TEXT
+  gender TEXT,
   color TEXT,
-  temperament TEXT
+  temperament TEXT,
   alive BOOLEAN
 );
